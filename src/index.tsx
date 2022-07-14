565c1ed1aa2c8bc3056f994e2800fecfe6104115
landhyun <wlgusdbr22@naver.com>
2022-07-14 19:27:56 +0900

    #4.1 fin

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
-//import { darkTheme } from './theme';
 import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-    textColor: "black",
-    bgColor: "white",
-    btnColor: "teal"
+    textColor: "white",
+    bgColor: "#231955",
+    accentColor: "#E8AA42"
 };
